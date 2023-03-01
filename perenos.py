12d4f6b8690cd24f5f150e50fea426537349e8ed
Neko1313 <89246763535@mail.ru>
2023-03-02 03:25:44 +1000

    first commit

---
+++ perenos.py
@@ -0,0 +1,24 @@
+import sqlite3
+con1 = sqlite3.connect("/home/tets/strashoeDelo.db")
+
+cur1 = con1.cursor()
+
+ad = []
+
+for row in cur1.execute("SELECT * FROM `us`"):
+    row = list(row)
+    if row[2] == None:
+        row[2] = "-1"
+    row.append(row[1])
+    row = tuple(row)
+    ad.append(row)
+
+con2 = sqlite3.connect("/home/HelloDjang/db.sqlite3")
+
+cur2 = con2.cursor()
+
+for row in ad:
+    con2.execute("INSERT INTO `user_SD_usersd` (`id`,`userid`, `refid`,`souls`,`ref`,`name_user`) VALUES (?,?,?,?,?,?)",row)
+
+for row in cur2.execute("SELECT * FROM `user_SD_usersd`"):
+    print(row)
